2b0040a4dab13ed82afa1b2dc0aa0b3e63b2f7c2
Hapikc <136709525532hs@gmail.com>
2025-02-11 19:36:23 +0700

    card add

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
         template: '<div>Kanban Board</div>'
     });
 
+    Vue.component('kanban-card', {
+        props: ['card'],
+        template: `
+        <div class="kanban-card">
+            <h3>{{ card.title }}</h3>
+            <p>{{ card.description }}</p>
+            <p>Дедлайн: {{ card.deadline }}</p>
+        </div>
+    `
+    });
+
 });
